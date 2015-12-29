49fe74e02cbac24d283bf59aa233bd81b7f3dca5
Edison Su <edison.su@trantect.com>
2015-12-29 23:34:45 +0800

    rm run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
-
-sed -i "s;^elasticsearch_url:.*;elasticsearch_url: ${KIBANA_ES_URL};" "/app/config/kibana.yml"
-sed -i "s;^default_app_id:.*;default_app_id: dashboard/default-Dashboard;" "/app/config/kibana.yml"
-exec /app/bin/kibana
